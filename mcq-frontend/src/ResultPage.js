7933299e83103a496185f0895f6578b7a4d1c2e0
Loki <112240321+Lokeswar-22@users.noreply.github.com>
2024-06-01 11:25:05 +0530

    push

--- mcq-frontend/src/ResultPage.js
+++ mcq-frontend/src/ResultPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import './ResultPage.css'; // Import the CSS file
+import './ResultPage.css'; 
 
 const ResultPage = () => {
   const { candidateName } = useParams();
